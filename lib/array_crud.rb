1da84568722ef7cf049a10b3a63c2dbdcc503b79
Austin Buhler <austinbuhler@gmail.com>
2020-03-15 21:18:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-
+  array = []
+  element = 1000
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
